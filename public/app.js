0205139b90af35b08a2d054aa8187f20bcb0a32c
Kinsey Ashby <kinseyzaire@gmail.com>
2015-12-08 22:23:22 -0700

    started in on playlist.html

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// var ccchoose = $("#chooseEm")
+//
+// ccchoose.click(function() {
+// var lookie = $.ajax( {
+//     url: "https://lit-fortress-6467.herokuapp.com/object",
+//     dataType: "json",
+//     success: function(data) {
+//       var covers = results[i][cover_art]
+//     }
+// });
