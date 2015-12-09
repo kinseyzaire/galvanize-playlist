c735e4d992fcb0ce6dbf36604b6a84255a7e8ec9
Kinsey Ashby <kinseyzaire@gmail.com>
2015-12-09 11:33:38 -0700

    last commit of mod 1

--- public/app.js
+++ public/app.js
@@ -1,3 +1,19 @@
+var randomCovers = $("#randAlbums")
+
+$(document).ready(function(){
+   var threeCovers = $.ajax( {
+      url:"https://lit-fortress-6467.herokuapp.com/object",
+      method:"GET",
+      dataType: "json",
+      success: function(data) {
+      var albumsss = data.results[0]["cover_art"];
+      randomCovers.html('<img src="images/'+ data.results[0]["cover_art"] +'" width="240px" height="240px alt="album cover"><br><img src="images/'+ data.results[1]["cover_art"] +'" width="240px" height="240px alt="album cover"><br><img src="images/'+ data.results[2]["cover_art"] +'" width="240px" height="240px alt="album cover"><br>')
+      }
+   });
+});
+
+
+
 // var ccchoose = $("#chooseEm")
 //
 // ccchoose.click(function() {
